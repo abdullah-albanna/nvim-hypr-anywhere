9f054f830de48ab8524807b96da5556798ad0b79
abdullah-albanna <abdu.albanna@proton.me>
2025-09-13 15:29:27 +0300

    initial script

---
+++ nvim-anywhere.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -euo pipefail
+
+check_deps() {
+	for cmd in wl-copy nvim; do
+		if ! command -v "$cmd" &>/dev/null; then
+			echo "Error: $cmd is required but not installed."
+			exit 1
+		fi
+	done
+}
+
+check_deps
+
+TMPFILE_DIR="/tmp/nvim-hypr-anywhere/"
+TMPFILE=$TMPFILE_DIR/doc-$(date +"%y%m%d%H%M%S")
+
+# make sure the tmp dir exists
+mkdir -p $TMPFILE_DIR
+
+# probably not needed but any way
+touch "$TMPFILE"
+
+# only yours
+chmod og-rwx "$TMPFILE"
+
+# you probably want to type stright away, so start at insert
+#
+# also, once you save it, it exits
+nvim +startinsert +'autocmd BufWritePost <buffer> quit' "$TMPFILE"
+
+TEXT=$(<"$TMPFILE")
+
+# skip if it's empty
+if [[ -n "$TEXT" ]]; then
+	echo "$TEXT" | wl-copy
+fi
+
+# if you want to delete it once done with it
+#rm -rf "$TMPFILE"
